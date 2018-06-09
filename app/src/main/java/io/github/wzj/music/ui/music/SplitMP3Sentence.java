3b38b4d768d129a4cb264d358479a68bfa30fce6
wuzijiang <wzj_whut@163.com>
2018-06-09 23:19:57 +0800

    添加了一个示例图片

--- app/src/main/java/io/github/wzj/music/ui/music/SplitMP3Sentence.java
+++ app/src/main/java/io/github/wzj/music/ui/music/SplitMP3Sentence.java
@@ -12,6 +12,8 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by senshan_wang on 2016/3/31.
@@ -28,11 +30,13 @@ public class SplitMP3Sentence {
     private OnProgressListener onProgressListener;
     private Thread extractThread;
     private Thread decodeThread;
-    private ArrayList<Integer> amptitudes;
-    private int seconds;
-    private ArrayList<SentenceBreakPoint> breakpoints = new ArrayList<SentenceBreakPoint>();
-    private ArrayList<Integer> pointsList = new ArrayList<>();
 
+    private int seconds;
+    private List<SilencePoint> silencePoints = new LinkedList<SilencePoint>();
+    private List<Integer> splitPointsList = new LinkedList<>();
+
+    /** 静音的振幅阈值 */
+    final int VAD_THROLSHOLD = (int) (Short.MAX_VALUE* 0.025 );
     /**
      * 转码完成回调接口
      */
@@ -48,17 +52,25 @@ public class SplitMP3Sentence {
 
     private Handler handler = new Handler();
 
-    public static class SentenceBreakPoint{
-        public int pos;
-        public int dur;
+    public static class SilencePoint {
+        /** 静音开始的位置, 以毫秒为单位 */
+        public final int posMS;
+        /** 静音持续的时间, 以毫秒为单位 */
+        public final int durMS;
+        public SilencePoint(int p, int d){
+            this.posMS = p;
+            this.durMS = d;
+        }
     }
 
     public SplitMP3Sentence(String filepath){
         this.srcPath = filepath;
+        /** 方便算法处理 */
+        silencePoints.add(new SilencePoint(0, 0));
     }
 
-    public ArrayList<Integer> getPointsList() {
-        return pointsList;
+    public List<Integer> getSplitPointsList() {
+        return splitPointsList;
     }
 
     public void start(){
@@ -88,8 +100,6 @@ public class SplitMP3Sentence {
 
                         logger.info("length in seconds: {}", seconds);
 
-
-                        amptitudes = new ArrayList<Integer>(seconds*50);
                         extractThread = new Thread(new Runnable() {
                             @Override
                             public void run() {
@@ -127,130 +137,59 @@ public class SplitMP3Sentence {
                     }
                     release();
                 }
-                logger.info("complete");
+                logger.info("silence point count: {}", silencePoints.size());
 
-
-                final int throshold = (int) (Short.MAX_VALUE* 0.025 );
-                if(amptitudes != null && amptitudes.size()>0) {
-                    /** 保存语句断点, 单位为毫秒. 振幅为每20毫秒计算一次
-                     *  断句的规则:
-                     *  超过200这毫秒的停顿, 认为是一个疑似断点, 但可能是一个假断点
-                     */
-
-                    /** 出现疑似静音的位置 */
-                    int silenceBeginIndex = 0;
-                    /** 静音的数量 */
-                    int silenceCount = 0;
-
-                    /** 非静音段的数量 */
-                    int voiceCount = 0;
-
-                    /** 如果为true, 表示当前处于正在处理静音的模式 */
-                    boolean silenceMode = false;
-
-                    {
-                        SentenceBreakPoint point = new SentenceBreakPoint();
-                        point.pos = 0;
-                        point.dur = 0;
-                        breakpoints.add(point);
-                    }
-
-                    int curIndex = 0;
-                    for (Integer am : amptitudes) {
-                        if(am<throshold){
-                            //没有声音,可能是个断点.
-                            if(silenceMode){
-                                silenceCount++;
-                            }else{
-                                //切换到静音处理模式.
-                                silenceMode = true;
-                                silenceCount = 1;
-                                silenceBeginIndex = curIndex;
-                            }
-                        }else{
-                            //有声音
-                            if(silenceMode){
-                                //退出静音模式
-                                silenceMode = false;
-                                if(silenceCount<=2){
-                                    /** 太短, 很可能不是一个断点 */
-                                }else{
-                                    /** 很可能是一个断点 */
-                                    SentenceBreakPoint point = new SentenceBreakPoint();
-                                    point.pos = silenceBeginIndex * 20;
-                                    point.dur = silenceCount * 20;
-                                    breakpoints.add(point);
-                                    logger.info("breaks {}", breakpoints.size());
-                                }
-                            }else{
-
-                            }
-                        }
-                        curIndex++;
-                    }
-                }
-                logger.info("break point count: {}", breakpoints.size());
-                pointsList = new ArrayList<Integer>();
-
+                /** silencePoints中的数据仅仅是可能的停顿点，需要对这些停顿点再次处理, 拆分出长度合适的语句  */
                 int voiceLength = 0;
-                ArrayList<SentenceBreakPoint> temp = new ArrayList<>();
-                //ArrayList<Break> result = new ArrayList<>();
-                for(int i=1; i<breakpoints.size(); i++){
-                    SentenceBreakPoint last = breakpoints.get(i-1);
-                    SentenceBreakPoint cur = breakpoints.get(i);
-                    int len = cur.pos - last.pos - last.dur;
+                //ArrayList<SilencePoint> temp = new ArrayList<>();
+
+                SilencePoint maxSilentPoint = null;
+                int maxSilenceIndex = 1;
+
+                /** 第一个点，就是语音起始点0秒的时候, 长度也是0; 从第二个点开始计算每段的语音长度 */
+                for(int i = 1; i< silencePoints.size(); i++){
+                    SilencePoint last = silencePoints.get(i-1);
+                    SilencePoint cur = silencePoints.get(i);
+
+                    /** 当前停顿点与上一个停顿点之间的语音长度 */
+                    int len = cur.posMS - last.posMS - last.durMS;
+                    /** 语音已经累计的长度 */
                     voiceLength += len;
                     if(voiceLength < 3000){
+
+                        /** 3秒的语音太短了, 继续找一下停顿点 */
                         continue;
                     }else if(voiceLength < 8000){
-                        temp.add(cur);
-                    }else{//>=10000
-                        temp.add(cur);
-                        SentenceBreakPoint max = temp.get(0);
-                        int fitIndex = 0;
-                        for(int j=0; j<temp.size(); j++){
-                            SentenceBreakPoint b = temp.get(j);
-                            if(max.dur<b.dur){
-                                max = b;
-                                fitIndex = j;
-                            }
+                        if(maxSilentPoint == null){
+                            maxSilenceIndex = i;
+                            maxSilentPoint = cur;
+                        }
+                        /**3秒到8秒之间的停顿点，先记录下来. 等语句累计到一定程度, 再找出最合适的点 */
+                        if(maxSilentPoint.durMS < cur.durMS){
+                            maxSilentPoint = cur;
+                            maxSilenceIndex = i;
+                        }
+                    }else{ //超过了8秒, 理论上可以找出合适的停顿点了
+                        if(maxSilentPoint == null){
+                            maxSilenceIndex = i;
+                            maxSilentPoint = cur;
                         }
 
-                        if(voiceLength < 15000 && max.dur<=60){
+                        if(voiceLength < 15000 && maxSilentPoint.durMS <=60){
+                            /** 最长停顿点还是很短, 继续找. 如果达到了15秒, 语句太长, 则不再找了 */
                             continue;
                         }
 
-                        pointsList.add(max.pos + (max.dur /2));
-                        logger.info("points: {}", pointsList.size());
+                        splitPointsList.add(maxSilentPoint.posMS + (maxSilentPoint.durMS /2));
+                        //logger.info("points: {}", splitPointsList.size());
                         voiceLength = 0;
-                        i = i - ( temp.size() - fitIndex ) + 1;
-                        temp.clear();
+                        maxSilentPoint = null;
+                        i = maxSilenceIndex;
                     }
                 }
-
-
-//                int voiceLength = breakpoints.size()==0?0:breakpoints.get(0).pos;
-//                for(int i=0; i<breakpoints.size()-1; i++){
-//                    SentenceBreakPoint cur = breakpoints.get(i);
-//                    SentenceBreakPoint next = breakpoints.get(i+1);
-//                    int thizVoiceLength = next.pos - cur.pos - cur.dur;
-//
-//                    if((voiceLength>=5000 && cur.dur >=350) || (voiceLength>=8000)){
-//                        /** 语音太长了, 当前就是一个断点 */
-//                        pointsList.add(cur.pos + (cur.dur /2));
-////                        logger.info("[Break]voiceLength: {}, thisVoidelen: {}, cur pos: {}, cur dur: {}, nex pos: {}, next dur: {}",
-////                                voiceLength, thizVoiceLength, cur.positionMS, cur.durationMS, next.positionMS, next.durationMS);
-//                        voiceLength = thizVoiceLength;
-//                    }else{
-//                        voiceLength += thizVoiceLength;
-////                        logger.info("[None] voiceLength: {}, thisVoidelen: {}, cur pos: {}, cur dur: {}, nex pos: {}, next dur: {}",
-////                                voiceLength, thizVoiceLength, cur.positionMS, cur.durationMS, next.positionMS, next.durationMS);
-//                    }
-//                }
-
-//                for(Integer point : pointsList){
-//                    logger.info("point: {}", point);
-//                }
+                for(Integer pos : splitPointsList){
+                    logger.info("split point: {}", pos);
+                }
 
                 handler.post(new Runnable() {
                     @Override
@@ -287,14 +226,12 @@ public class SplitMP3Sentence {
             logger.info("sample rate: {}, channel count: {}", sampleRate, channelCount);
 
             if (mime.startsWith("audio")) {//获取音频轨道
-//                    format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 200 * 1024);
                 mediaExtractor.selectTrack(i);//选择此音频轨道
                 mediaDecode = MediaCodec.createDecoderByType(mime);//创建Decode解码器
                 mediaDecode.configure(format, null, null, 0);
                 break;
             }
         }
-
         if (mediaDecode == null) {
             throw new RuntimeException("get audio track failed");
         }
@@ -331,18 +268,32 @@ public class SplitMP3Sentence {
     }
 
     private void drain() {
-        //获取解码得到的byte[]数据 参数BufferInfo上面已介绍 10000同样为等待时间 同上-1代表一直等待，0代表不等待。此处单位为微秒
-        //此处建议不要填-1 有些时候并没有数据输出，那么他就会一直卡在这 等待
         ByteBuffer[] decodeOutputBuffers = mediaDecode.getOutputBuffers();//MediaCodec在此ByteBuffer[]中获取输入数据
         MediaCodec.BufferInfo decodeBufferInfo = new MediaCodec.BufferInfo();
-        byte[] pcm = null;
-        short sample;
-        int sampleCount = 0;
-        long sum = 0;
-        int max = 0;
+
         /** 每20毫秒中的采样数*/
-        final int samplesPer20MS = sampleRate/50*channelCount;
-        final short[] tempBuffer = new short[samplesPer20MS];
+        final int sampleCountPer20MS = sampleRate/50*channelCount;
+
+        /** 临时存放解码出的pcm数据 */
+        byte[] pcmBytes = null;
+        /** 累计到20MS时, 计算振幅的平均值 */
+        int sampleCount = 0;
+        /** 累计振幅 */
+        long samplesSum = 0;
+
+        /** 出现静音的位置, 以20MS为单位 */
+        int silence20msPos = 0;
+        /** 已经处理了的数据包的个数, 每一个为20MS */
+        int packet20msNumber = 0;
+
+        /** 语音持续的长度, 以20MS为单位 */
+        int voiceLength = 0;
+
+        /** 静音持续的长度 */
+        int silenceLength = 0;
+
+        /** 上一次的音频是否为静音, 用于判断静音与非静音状态的转换 */
+        boolean isSilenceMode = false;
 
         while(true) {
             int outputIndex = mediaDecode.dequeueOutputBuffer(decodeBufferInfo, 100000);
@@ -356,26 +307,47 @@ public class SplitMP3Sentence {
             }
             ByteBuffer outputBuffer = decodeOutputBuffers[outputIndex];//拿到用于存放PCM数据的Buffer
             totalSampleCount += decodeBufferInfo.size >> 1;
-            if(pcm == null || pcm.length < decodeBufferInfo.size){
-                pcm = new byte[decodeBufferInfo.size];
+            if(pcmBytes == null || pcmBytes.length < decodeBufferInfo.size){
+                pcmBytes = new byte[decodeBufferInfo.size];
             }
-            outputBuffer.get(pcm);//将Buffer内的数据取出到字节数组中
+            outputBuffer.get(pcmBytes);//将Buffer内的数据取出到字节数组中
             outputBuffer.clear();//数据取出后一定记得清空此Buffer MediaCodec是循环使用这些Buffer的，不清空下次会得到同样的数据
             mediaDecode.releaseOutputBuffer(outputIndex, false);//此操作一定要做，不然MediaCodec用完所有的Buffer后 将不能向外输出数据
 
+            /** 每20毫秒计算一次平均振幅 */
             for(int i=0; i<decodeBufferInfo.size; i+=2){
-                sample = (short)((pcm[i] & 0xff) | ( (pcm[i+1]&0xff)<<8));
                 sampleCount++;
-                sum += Math.abs(sample);
-                max = Math.max(max, Math.abs(sample));
-                if(sampleCount == samplesPer20MS){
-                    int average = (int)(sum/samplesPer20MS);
-                    //average = (average + max)/2;
-                    amptitudes.add(average);
-                    //amptitudes.add(v);
-                    sum = 0;
-                    max = 0;
+                samplesSum += Math.abs((short)((pcmBytes[i] & 0xff) | ( (pcmBytes[i+1]&0xff)<<8)));
+                if(sampleCount == sampleCountPer20MS){
+                    int average = (int)(samplesSum /sampleCountPer20MS);
+                    samplesSum = 0;
                     sampleCount = 0;
+
+                    /** 静音检测 */
+                    if(average < VAD_THROLSHOLD){
+                        /** 声音太小, 认为是静音 */
+                        if(isSilenceMode){
+                            /** 持续静音 */
+                            silenceLength++;
+                        }else{
+                            /** 由语音模式转换为静音模式 */
+                            isSilenceMode = true;
+                            silenceLength = 1;
+                            silence20msPos = packet20msNumber;
+                        }
+                    }else{/** 有声音 */
+                        if(isSilenceMode){
+                            /** 由静音模式转换为语音 */
+                            isSilenceMode = false;
+                            if(silenceLength<=2){
+                                /** 太短, 不认为是一个断点 */
+                            }else{
+                                /** 很可能是一个断点 */
+                                this.silencePoints.add(new SilencePoint(silence20msPos*20, silenceLength*20));
+                            }
+                        }
+                    }
+                    packet20msNumber++;
                 }
             }
         }
